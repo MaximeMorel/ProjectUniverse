2dc6172b31b508d20dba4ad591162f8a6c2d1cd9
MaximeMorel <maxime.morel69@gmail.com>
2014-06-17 23:09:01 +0200

    window null

--- projects/premake4.lua
+++ projects/premake4.lua
@@ -21,20 +21,26 @@ solution "ProjectUniverse"
    project "RenderOpenGL4"
       kind "SharedLib"
       files { "../src/core/render/renderOpenGL4/**.hpp", "../src/core/render/renderOpenGL4/**.cpp" }
+      files { "../src/core/render/windowContext/windowContextSDL2/**.hpp", "../src/core/render/windowContext/windowContextSDL2/**.cpp" }
+      buildoptions {"`pkg-config --cflags cppunit lua sdl2 gl glew`" }
+      linkoptions  {"`pkg-config --libs   cppunit lua sdl2 gl glew`" }
 
    project "RenderNull"
       kind "SharedLib"
       files { "../src/core/render/renderNull/**.hpp", "../src/core/render/renderNull/**.cpp" }
+      files { "../src/core/render/windowContext/windowContextNull/**.hpp", "../src/core/render/windowContext/windowContextNull/**.cpp" }
 
    -- A project defines one build target
    project "ProjectUniverse"
       kind "ConsoleApp"
       --language "C++"
       files { "../src/**.hpp", "../src/**.cpp", "../src/**.inl" }
-      --links { "GL", "GLEW" }
-      links { "RenderOpenGL4" }
-      --buildoptions { "-std=c++11" }
-      --includedirs { "../src/" }
-      buildoptions {"`pkg-config --cflags cppunit lua sdl2 gl glew`" }
-      linkoptions  {"`pkg-config --libs   cppunit lua sdl2 gl glew`" }
+      excludes { "../src/core/render/renderOpenGL4/**.hpp", "../src/core/render/renderOpenGL4/**.cpp" }
+      excludes { "../src/core/render/windowContext/windowContextSDL2/**.hpp", "../src/core/render/windowContext/windowContextSDL2/**.cpp" }
+      excludes { "../src/core/render/renderNull/**.hpp", "../src/core/render/renderNull/**.cpp" }
+      --links { "RenderOpenGL4" }
+      links { "RenderNull" }
+      buildoptions {"`pkg-config --cflags cppunit lua`" }
+      linkoptions  {"`pkg-config --libs   cppunit lua`" }
+      linkoptions { "-Wl,-rpath=." }
       flags { "ExtraWarnings" }
