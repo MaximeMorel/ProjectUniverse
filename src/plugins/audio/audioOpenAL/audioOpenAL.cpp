aa4c6c12bd84fd4633582129982ddef184e72bbd
Maxime Morel <maxime@mmorel.eu>
2016-04-21 16:45:24 +0100

    OpenAL list devices

--- src/plugins/audio/audioOpenAL/audioOpenAL.cpp
+++ src/plugins/audio/audioOpenAL/audioOpenAL.cpp
@@ -1,7 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include "audioOpenAL.hpp"
-#include <AL/al.h>
-#include <AL/alc.h>
 ////////////////////////////////////////////////////////////////////////////////
 PluginInfo pluginInfo = { "audioOpenAL",
                           "audioOpenAL",
@@ -31,16 +29,46 @@ void closeLibInstance()
 }
 ////////////////////////////////////////////////////////////////////////////////
 PluginAudioOpenAL::PluginAudioOpenAL(Engine &engine)
-    : Library(engine)
+    : Library(engine), m_device(nullptr)
 {
     log().log() << "PluginAudioOpenAL start..." << std::endl;
-    ALCdevice *device;
-    device = alcOpenDevice(NULL);
-    alcCloseDevice(device);
+
+    std::vector<std::string> list = listDevices();
+    if (!list.empty())
+    {
+        log().log() << "OpenAl devices:\n";
+        for (const auto& str : list)
+        {
+            log().log() << str << "\n";
+        }
+
+        log().log() << "Using device: " << list[0].c_str() << std::endl;
+        m_device = alcOpenDevice(list[0].c_str());
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////
 PluginAudioOpenAL::~PluginAudioOpenAL()
 {
     log().log() << "PluginAudioOpenAL stop..." << std::endl;
+    if (m_device)
+    {
+        alcCloseDevice(m_device);
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+std::vector<std::string> PluginAudioOpenAL::listDevices() const
+{
+    const ALCchar *devices = alcGetString(NULL, ALC_DEVICE_SPECIFIER);
+    const ALCchar *device = devices, *next = devices + 1;
+    size_t len = 0;
+    std::vector<std::string> res;
+    while (device && *device != '\0' && next && *next != '\0')
+    {
+        res.push_back(device);
+        len = res.back().length();
+        device += (len + 1);
+        next += (len + 2);
+    }
+    return res;
 }
 ////////////////////////////////////////////////////////////////////////////////
