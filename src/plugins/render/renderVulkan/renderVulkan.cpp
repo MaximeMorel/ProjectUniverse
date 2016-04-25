f3fc5a05242a44dad2b386a0c91ad1619b161d54
Maxime Morel <maxime@mmorel.eu>
2016-04-25 17:18:19 +0100

    Vulkan test

--- src/plugins/render/renderVulkan/renderVulkan.cpp
+++ src/plugins/render/renderVulkan/renderVulkan.cpp
@@ -31,9 +31,49 @@ void closeLibInstance()
 PluginRenderVulkan::PluginRenderVulkan(Engine &engine)
     : Library(engine)
 {
+    log().log() << "PluginRenderVulkan start..." << std::endl;
+
+    // Filling out application description:
+    // sType is mandatory
+    m_applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    // pNext is mandatory
+    m_applicationInfo.pNext = nullptr;
+    // The name of our application
+    m_applicationInfo.pApplicationName = "app";
+    // The name of the engine (e.g: Game engine name)
+    m_applicationInfo.pEngineName = nullptr;
+    // The version of the engine
+    m_applicationInfo.engineVersion = 1;
+    // The version of Vulkan we're using for this application
+    m_applicationInfo.apiVersion = VK_API_VERSION;
+
+    // Filling out instance description:
+    // sType is mandatory
+    m_instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    // pNext is mandatory
+    m_instanceInfo.pNext = nullptr;
+    // flags is mandatory
+    m_instanceInfo.flags = 0;
+    // The application info structure is then passed through the instance
+    m_instanceInfo.pApplicationInfo = &m_applicationInfo;
+    // Don't enable and layer
+    m_instanceInfo.enabledLayerCount = 0;
+    m_instanceInfo.ppEnabledLayerNames = nullptr;
+    // Don't enable any extensions
+    m_instanceInfo.enabledExtensionCount = 0;
+    m_instanceInfo.ppEnabledExtensionNames = nullptr;
+
+    // Now create the desired instance
+    vkResult result = vkCreateInstance(&m_instanceInfo, nullptr, &m_instance);
+    if (result != VK_SUCCESS)
+    {
+        log().log() << "Failed to create instance:" << result << std::endl;
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////
 PluginRenderVulkan::~PluginRenderVulkan()
 {
+    log().log() << "PluginRenderVulkan stop..." << std::endl;
+    vkDestroyInstance(m_instance, nullptr);
 }
 ////////////////////////////////////////////////////////////////////////////////
